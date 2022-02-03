33e53992fdccca7a82cdb8786acd180bd34bbf3c
Com-Sun <gnlslfflffl@gmail.com>
2022-02-03 09:45:20 +0900

    feat: naver 오픈 api 추가 전 백업

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ client = MongoClient('localhost', 27017)
 db = client.comsun
 app = Flask(__name__)
 
+# open API
+import urllib.request
+
 
 # HTML을 주는 부분
 @app.route('/')
@@ -52,6 +55,22 @@ def post_list():
 
     return jsonify({'result': 'success', 'msg': 'POST 연결되었습니다!'})
 
+
+client_id = "YOUR_CLIENT_ID"
+client_secret = "YOUR_CLIENT_SECRET"
+encText = urllib.parse.quote("검색할 단어")
+url = "https://openapi.naver.com/v1/search/blog?query=" + encText  # json 결과
+# url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
+request = urllib.request.Request(url)
+request.add_header("X-Naver-Client-Id", client_id)
+request.add_header("X-Naver-Client-Secret", client_secret)
+response = urllib.request.urlopen(request)
+rescode = response.getcode()
+if (rescode == 200):
+    response_body = response.read()
+    print(response_body.decode('utf-8'))
+else:
+    print("Error Code:" + rescode)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
